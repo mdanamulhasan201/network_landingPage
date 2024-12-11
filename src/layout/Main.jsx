78e208e35c05bb5f284e0947d5345182fa2a1919
Md. Anamul Hasan <mdanamulhasan.cse@gmail.com>
2024-12-11 23:44:03 +0600

    added

---
+++ src/layout/Main.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+
+
+const Main = () => {
+    return (
+        <div>
+            <h1>this navbar</h1>
+            <Outlet />
+            <h1>Footer</h1>
+        </div>
+    );
+};
+
+export default Main;
