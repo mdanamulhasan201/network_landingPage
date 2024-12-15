4036483c090a95bda1fd0056acb7cd2e18edfac4
Md. Anamul Hasan <mdanamulhasan.cse@gmail.com>
2024-12-16 04:21:30 +0600

    footer added

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     const currentYear = new Date().getFullYear(); 
 
     return (
-        <div className="text-center py-4 bg-gray-100 text-gray-600 font-heebo">
+        <div className="text-center py-10 bg-slate-50 text-gray-600 font-heebo">
             © {currentYear} Media Cash. All rights reserved.
         </div>
     );
