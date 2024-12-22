7d0a4517c3ef05c8fd638817642eae66f8111b5e
Md. Anamul Hasan <mdanamulhasan.cse@gmail.com>
2024-12-22 17:32:36 +0600

    update banner section title

--- src/components/Home/AboutUs.jsx
+++ src/components/Home/AboutUs.jsx
@@ -41,7 +41,7 @@ const AboutUs = () => {
                     Dedicated to Delivering Value
                 </h1>
                 <p className="font-heebo text-lg mt-5">
-                    <span className="font-semibold">M Cash Media</span>  stands as a leading growth partner, taking a strategic approach to performance marketing. As a results-driven and dynamic agency, we focus on delivering measurable, data-backed results. Our team of experienced professionals brings a business-oriented perspective, committed to driving your success with accuracy and expertise.
+                    <span className="font-semibold">MCash Media</span>  stands as a leading growth partner, taking a strategic approach to performance marketing. As a results-driven and dynamic agency, we focus on delivering measurable, data-backed results. Our team of experienced professionals brings a business-oriented perspective, committed to driving your success with accuracy and expertise.
                     To run a successful performance marketing campaign, delivering the right message through the right channels is key. Whether you aim to acquire new customers, generate leads, or drive meaningful customer actions, we provide the tools, expertise, and talent to achieve your goals.
                 </p>
             </div>
